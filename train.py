90e0db06428e97f0be10baae7296f6855e7b0617
Henrik Fridström <HenkeF1@outlook.com>
2020-11-11 00:26:12 +0100

    first try

--- train.py
+++ train.py
@@ -9,20 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+from azureml.core import Dataset
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,9 +36,18 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
 
 def main():
+    ds = Dataset.Tabular.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+
+    x, y = clean_data(ds)
+
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.30)
+
+    run = Run.get_context()
+        
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
@@ -68,5 +64,11 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    os.makedirs('outputs', exist_ok=True)
+    # note file saved in the outputs folder is automatically uploaded into experiment record
+    joblib.dump(value=model, filename='outputs/model.pkl')
+
+    run.complete()
+    
 if __name__ == '__main__':
     main()
